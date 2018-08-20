2e83ace72086dea6ffa408607427d8b5631e5fc9
chhatbarnikita <nikichhatbar17@gmail.com>
2018-08-20 15:49:36 +0530

    Finished decryption

--- columnar.py
+++ columnar.py
@@ -1,24 +1,37 @@
 import numpy as np
 
 
-def encrypt():
+def encrypt(text):
     encrypted_text = []
-    ls = [c for c in key] + [l for l in plaintext]
-    rem = np.remainder(len(plaintext), len(key))
+    ls = [c for c in key] + [l for l in text]
+    rem = np.remainder(len(text), len(key))
     if rem != 0:
-        ls.extend('q' * rem)
+        ls.extend('q' * (len(key) - rem)) #appending list for proper length
     for i in range(1, len(key)+1):
         index = ls.index(str(i)) + len(key)
-        encrypted_text += [ls[c] for c in range(index, len(ls), len(key))]
+        encrypted_text += [ls[c] for c in range(index, len(ls), len(key))] #adding column values
     return "".join(encrypted_text)
 
 
 def decrypt(text):
-    return
+    temp = dict()
+    x = 0
+    decrypted_text = ''
+    column_size = int(len(text) / len(key))
+    for i in range(1, len(key)+1):
+        temp.__setitem__(i, text[x: x+column_size]) #converting to a dictionary
+        x += column_size
+    for i in range(column_size):
+        for c in key:
+            decrypted_text += temp.get(int(c))[i]
+    return decrypted_text.strip('q')
 
 
 if __name__ == '__main__':
+    # key = input("Enter key (in digits): ")
+    # plaintext = input("Enter text: ")
+    key = "253164"
     plaintext = "nikitachhatbar"
-    key = "4213"
-    cipher_text = encrypt()
-    print(cipher_text)
+    cipher_text = encrypt(plaintext)
+    print("Encrypted text: " + cipher_text)
+    print("Decrypted text: " + decrypt(cipher_text))
