38a48c249bc474fa2b08b523a8bb3cd7ae9b912a
chhatbarnikita <nikichhatbar17@gmail.com>
2018-09-26 22:42:53 +0530

    Finished decryption

--- rsa.py
+++ rsa.py
@@ -5,33 +5,36 @@ def totient(l, m):
     return (l-1)*(m-1)
 
 
-def find_publickey(a, m):
-    a = np.remainder(a, m)
-    for x in range(1, m):
-        if np.remainder((a * x), m) == 1:
-            return x
-    return 1
+def find_publickey(r1, r2):
+    r, t, t1, t2, q1 = 0, 0, 1, 0, 0
+    while r != 1:
+        r = np.remainder(r2, r1)
+        q1 = int(r2 / r1)
+        t = t2 - (q1 * t1)
+        t2, t1 = t1, t
+        r2, r1 = r1, r
+    return t
 
 
 def convert(key, product, msg):
-    converted_text = np.remainder(msg**key, product)
+    num = np.remainder(msg, product)
+    for i in range(0, key - 1):
+        num *= msg
+        num = np.remainder(num, product)
+    converted_text = num
     return converted_text
 
 
 if __name__ == '__main__':
-    # text = int(input("Enter text: "))
-    print(11**23)
+    # text = input("Enter text: ")
     text = 88
     p = 17
-    q = 11
+    q = 83
     n = p * q
     e = 7
     tot = totient(p, q)
-    print("n= " + str(n))
-    print("totient= " + str(tot))
     d = find_publickey(e, tot)
-    print("d: " + str(d))
     cipher_text = convert(e, n, text)
+    print("Message: 88")
     print("Encrypted text: " + str(cipher_text))
     print("Decrypted text: " + str(convert(d, n, cipher_text)))
-    print()
